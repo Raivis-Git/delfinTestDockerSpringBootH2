70446005b8672ad3c341d2d7aa86b6de817c2bcb
Raivis Treikals <raivis.treikals@banknote.lv>
2021-10-25 11:32:14 +0300

    clear

--- src/main/java/com/task/network/TestDelfinApplication.java
+++ src/main/java/com/task/network/TestDelfinApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NetworkApplication {
+public class TestDelfinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NetworkApplication.class, args);
+		SpringApplication.run(TestDelfinApplication.class, args);
 	}
 
 }
