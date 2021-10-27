790e7eae64a1697c4b733d8cccdf63fe1ce089e3
Raivis Treikals <raivis.treikals@banknote.lv>
2021-10-27 11:43:18 +0300

    data added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
 INSERT INTO r_client (first_name, last_name, personal_nr) VALUES
-  ('Liene', 'Ozola', '00-40-96-01-23-48');
+  ('Liene', 'Ozola', '10101010001'),
+  ('Francis', 'Bērziņš', '10101010002'),
+  ('Matilde', 'Ziemele', '10101010003'),
+  ('Nīna', 'Veczeme', '10101010004'),
+  ('Gustavs', 'Murāns', '10101010005'),
+  ('Klāvs', 'Spolītis', '10101010006'),
+  ('Liene', 'Asnate', '10101010007'),
+  ('Gustavs', 'Bērziņš', '10101010008');
 
